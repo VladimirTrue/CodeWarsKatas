77f7c96ef59f5c14117efec02941c93031835c82
DuglasWTF <mrforz93@outlook.com>
2021-09-17 19:54:53 +0300

    Sort the odd was accepted

---
+++ src/main/java/SortTheOdd.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortTheOdd {
+    public static void main(String[] args) {
+        int[] arr = sortArray(new int[]{5, 8, 6, 3, 4});
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static int[] sortArray(int[] array) {
+        List<Integer> buf = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                buf.add(array[i]);
+            }
+        }
+
+        Collections.sort(buf);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                for (int j = 0; j < buf.size(); j++) {
+                    array[i] = buf.get(j);
+                    break;
+                }
+                buf.remove(0);
+            }
+        }
+
+        return array;
+    }
+}
