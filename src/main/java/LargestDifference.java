6057c39091863a15a94d89e167ddf6cea396e58c
mrfor <truevladimir93Dev@yandex.ru>
2022-07-27 18:12:25 +0300

    add discription and refact largestifference

--- src/main/java/LargestDifference.java
+++ src/main/java/LargestDifference.java
@@ -1,17 +1,32 @@
-import java.util.*;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+/*
+https://www.codewars.com/kata/52503c77e5b972f21600000e
+
+Summary: Write a function which takes an array data of numbers and returns the largest difference in indexes j - i such that data[i] <= data[j]
+
+        Long Description:
+
+        The largestDifference takes an array of numbers. That array is not sorted. Do not sort it or change the order of the elements in any way, or their values.
+
+        Consider all of the pairs of numbers in the array where the first one is less than or equal to the second one.
+
+        From these, find a pair where their positions in the array are farthest apart.
+
+        Return the difference between the indexes of the two array elements in this pair.
+
+        Example:
+
+        [ 1, 2, 3] returns 2 because here j = 2 and i = 0 and 2 - 0 = 2
+
+*/
+
 
 public class LargestDifference {
     public static int largestDifference(int[] data) {
         int resIndex = 0;
-        for (int i = 0; i < data.length; i++) {
-            for (int j = i + 1; j < data.length; j++) {
-                if (data[i] <= data[j] && resIndex < j - i) {
-                    resIndex = j - i;
+        for (int left = 0; left < data.length; left++) {
+            for (int right = left + 1; right < data.length; right++) {
+                if (data[left] <= data[right] && resIndex < right - left) {
+                    resIndex = right - left;
                 }
             }
         }
