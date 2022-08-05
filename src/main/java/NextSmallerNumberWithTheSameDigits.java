f2600598f1fd6e53747772229f52f7005828b371
mrfor <truevladimir93Dev@yandex.ru>
2022-08-05 12:58:01 +0300

    complete

--- src/main/java/NextSmallerNumberWithTheSameDigits.java
+++ src/main/java/NextSmallerNumberWithTheSameDigits.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class NextSmallerNumberWithTheSameDigits {
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+public class NextSmallerNumberWithTheSameDigits {
+    public static long nextSmaller(long n) {
+
+        Set<Long> digitsInp = Arrays.stream(String.valueOf(n).split(""))
+                .mapToLong(Long::parseLong)
+                .boxed()
+                .collect(Collectors.toSet());
+
+        return LongStream.range(1, n)
+                .map(longValue -> n - longValue + 1 - 1)
+                .filter(dig -> new HashSet<>(Arrays.stream(String.valueOf(dig).split(""))
+                        .mapToLong(Long::parseLong)
+                        .boxed()
+                        .collect(Collectors.toList()))
+                        .containsAll(digitsInp)
+                        && digitsInp.containsAll(new HashSet<>(Arrays.stream(String.valueOf(dig).split(""))
+                        .mapToLong(Long::parseLong)
+                        .boxed()
+                        .collect(Collectors.toList()))
+                ))
+                .findFirst().orElse(-1);
+    }
 }
--- src/test/java/TestNextSmallerNumberWithTheSameDigitsTest.java
+++ src/test/java/TestNextSmallerNumberWithTheSameDigitsTest.java
@@ -1,4 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TestNextSmallerNumberWithTheSameDigitsTest {
-  
+
+    @Test
+    void nextSmaller() {
+        Assertions.assertEquals(123456789, NextSmallerNumberWithTheSameDigits.nextSmaller(123456798));
+
+        Assertions.assertEquals(790, NextSmallerNumberWithTheSameDigits.nextSmaller(907));
+
+        Assertions.assertEquals(414, NextSmallerNumberWithTheSameDigits.nextSmaller(441));
+
+        Assertions.assertEquals(12, NextSmallerNumberWithTheSameDigits.nextSmaller(21));
+        Assertions.assertEquals(513, NextSmallerNumberWithTheSameDigits.nextSmaller(531));
+        Assertions.assertEquals(-1, NextSmallerNumberWithTheSameDigits.nextSmaller(1027));
+    }
 }
